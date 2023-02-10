34491b4d368b7773e9cfc46fca9459f8ebdabe87
Sabryna Marques <scullysabrina8@gmail.com>
2023-02-09 22:21:49 -0300

    mostrando primeira pergunta

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//Dados Iniciais
+let currentQuestion = 0;
+
+showQuestion()
+
+
+//Funções
+function showQuestion(){
+    if(questions[currentQuestion]){
+        let q = questions[currentQuestion];
+
+        document.querySelector('.scoreArea').style.display =  'none';
+        document.querySelector('.questionArea').style.display =  'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+
+    } else {
+        //acabaram as questoes
+    }
+}
